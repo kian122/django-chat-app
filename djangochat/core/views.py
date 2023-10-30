f002e507b9289878a82f71fc3e53a03cc88fdc70
flawlessness-professional <mhrabykyan2@gmail.com>
2023-10-30 11:52:37 -0700

    adding logic

--- djangochat/core/views.py
+++ djangochat/core/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render , redirect
 
 # importing all the forms
-from . import form
+from .form import SignUpForm
 
 # importing login function
 from django.contrib.auth import login
 
 # Create your views here.
 
+# home page
 def frontpage(request):
     return render( request , "core/frontpage.html" )
+
+# sign up
+def signup( request):
+    # logic
+    if request.method == "POST":
+        form =  SignUpForm( request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login( request , user )
+
+            # Redirecting
+            return redirect('frontpage')
+    else:
+        form = SignUpForm()
+
+    return render( request , "core/signup.html" , {
+        "form": form
+    })
