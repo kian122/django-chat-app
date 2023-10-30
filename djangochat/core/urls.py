a7a06c31fbdf28382cb0ae30f17bbc248e98014d
flawlessness-professional <mhrabykyan2@gmail.com>
2023-10-30 11:51:23 -0700

    adding url

--- djangochat/core/urls.py
+++ djangochat/core/urls.py
@@ -1,6 +1,10 @@
-from .views import frontpage
+from .views import frontpage , signup
 from django.urls import path
 
 urlpatterns = [
+    # normal path
     path('', frontpage , name="frontpage"),
+
+    # sign up path
+    path('signup/', signup , name="signup"),
 ]
