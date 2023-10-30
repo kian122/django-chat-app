c697371bd418413856e053d7c5b7f6021d7b7f04
flawlessness-professional <mhrabykyan2@gmail.com>
2023-10-30 11:33:50 -0700

    importing

--- djangochat/core/views.py
+++ djangochat/core/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+
+# importing all the forms
+from . import form
+
+# importing login function
+from django.contrib.auth import login
 
 # Create your views here.
 
