fb882f136bbf94a3c2e9ae3a423b38baf9e0d2eb
flawlessness-professional <mhrabykyan2@gmail.com>
2023-10-31 06:42:23 -0700

    adding the log in function

--- djangochat/core/urls.py
+++ djangochat/core/urls.py
@@ -15,5 +15,5 @@ urlpatterns = [
     path('logout/' , auth_views.LogoutView.as_view() , name="logout"),
 
     # login url
-    path('login/' , auth_views.LoginView.as_view() , name="login")
+    path('login/' , auth_views.LoginView.as_view(template_name = "core/login.html") , name="login")
 ]
