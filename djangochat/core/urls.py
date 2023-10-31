22e29ce8ec79e0e956ad2082d333a33a88885df1
flawlessness-professional <mhrabykyan2@gmail.com>
2023-10-31 06:35:09 -0700

    log func

--- djangochat/core/urls.py
+++ djangochat/core/urls.py
@@ -1,10 +1,19 @@
 from .views import frontpage , signup
 from django.urls import path
 
+# log func import
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     # normal path
     path('', frontpage , name="frontpage"),
 
     # sign up path
     path('signup/', signup , name="signup"),
+
+    # logout url
+    path('logout/' , auth_views.LogoutView.as_view() , name="logout"),
+
+    # login url
+    path('login/' , auth_views.LoginView.as_view() , name="login")
 ]
