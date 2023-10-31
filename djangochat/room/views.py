b15fe8e305b0b5cadf82004a771c5f51f74d5d86
flawlessness-professional <mhrabykyan2@gmail.com>
2023-10-31 09:27:06 -0700

    room show func

--- djangochat/room/views.py
+++ djangochat/room/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+# import Room model
+from .models import Room
+
+# importing login func for security 
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def rooms(request):
+    # defining rooms
+    rooms = Room.objects.all()
+
+    return render( request , "room/rooms.html" , {
+        'rooms': rooms
+    })
+    
